01896be1d373ebef0b14ac98a93695b704422516
OneDoesNotSimplyProgram <onedoesnotsimplyprogram@outlook.com>
2015-07-30 22:52:17 -0700

    Small improvements
    Removed redundant code, added todo for extra credit work.  For now let's
    call this working.

--- PigLatin/PigLatin/Program.cs
+++ PigLatin/PigLatin/Program.cs
@@ -11,19 +11,10 @@ namespace PigLatin
         static void Main(string[] args)
         {
             Console.WriteLine("Hello.  I am a robot.  Bleep bloop.\nMy job is to convert stuff that you type to Pig Latin!\nType something for me!");
-            string userInput;
-            string userPigLatin;
-
-            userInput = GetUserInput();
-            userPigLatin = ConvertToPigLatin(userInput);
-            DisplayOutput(userPigLatin);
-        }
-
-        private static string GetUserInput()
-        {
             string userInput = Console.ReadLine();
-            
-            return userInput;
+            string userPigLatin = ConvertToPigLatin(userInput);
+            DisplayOutput(userPigLatin);
+            //TODO: implement mechanism that handles special characters and numbers
         }
 
         private static string ConvertToPigLatin(string userInput)
@@ -33,13 +24,11 @@ namespace PigLatin
 
             foreach (string word in userInputArray)
             {
-                bool userInputHasVowel = false;
                 string firstLetter = word.Substring(0,1).ToLower();
 
-                //determine if word has vowel
+                //distinguishes between vowel and consonant to satisfy the pig latin rules
                 if (firstLetter == "a" || firstLetter == "e" || firstLetter == "i" || firstLetter == "o" || firstLetter == "u")
                 {
-                    userInputHasVowel = true;
                     userPigLatin += word + "yay ";
                 }
                 else if (firstLetter == "b" || firstLetter == "c" || firstLetter == "d" || firstLetter == "f"
@@ -47,15 +36,10 @@ namespace PigLatin
                      || firstLetter == "l" || firstLetter == "m" || firstLetter == "n" || firstLetter == "p"
                      || firstLetter == "q" || firstLetter == "r" || firstLetter == "s" || firstLetter == "t"
                      || firstLetter == "v" || firstLetter == "w" || firstLetter == "x" || firstLetter == "y"
-                     || firstLetter == "z")
+                     || firstLetter == "z") //TODO: revise this later...kind of messy
                 {
-                    userInputHasVowel = false;
-                    string newWord = word.Substring(1, word.Length - 1);
-                    userPigLatin += newWord + firstLetter + "ay ";
-                }
-                else 
-                {
-                    break;
+                    string wordWithRemovedConsonant = word.Substring(1, word.Length - 1);
+                    userPigLatin += wordWithRemovedConsonant + firstLetter + "ay ";
                 }
             }
 
@@ -69,22 +53,4 @@ namespace PigLatin
         }
     }
 }
-
-/*  Rough design
-Assumptions
- 1) Intended for individual words without punctuation, numbers, or special characters
- 2) Only for converting TO pig latin, not converting from pig latin to english
-
-GetUserInput
-  • ReadLine from user
-  • Return string
-
-ConvertToPigLatin
-  • If begins with consonant, move first char to end of string and add 'ay' at the end of string
-  • Else if begins with vowel add 'yay' to end of string
-  • Else do nothing
-
-DisplayOutput
-  • Writeline of the output
-*/
 
