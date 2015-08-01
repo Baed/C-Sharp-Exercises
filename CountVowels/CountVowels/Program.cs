5d0b9117bd838a794e46473876c9d414c3c9ec92
OneDoesNotSimplyProgram <onedoesnotsimplyprogram@outlook.com>
2015-07-31 19:03:09 -0700

    Revert "Revert "Initial code""
    This reverts commit 255ccca3452bb08571fbdffd95dd98c54eacbdef.

---
+++ CountVowels/CountVowels/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CountVowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello.  I am a robot.  Bleep bloop.\nMy job is to count the vowels in what you type!\nType something for me!");
+            string userInput = Console.ReadLine();
+            
+            /* 
+             * 1) Split string into array by CHAR
+             * 2) Loop through array and count IF char is a vowel
+             * 3) Repeat 2 but store counts for each type of vowel for reporting
+            */
+        }
+    }
+}
