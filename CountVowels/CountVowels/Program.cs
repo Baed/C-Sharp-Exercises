466749e3fc2f79f287df065b58e1b8ef5da95dc1
OneDoesNotSimplyProgram <onedoesnotsimplyprogram@outlook.com>
2015-08-08 13:13:42 -0700

    Code for counting vowels
    Splits input into an array and counts the vowels

--- CountVowels/CountVowels/Program.cs
+++ CountVowels/CountVowels/Program.cs
@@ -12,12 +12,54 @@ namespace CountVowels
         {
             Console.WriteLine("Hello.  I am a robot.  Bleep bloop.\nMy job is to count the vowels in what you type!\nType something for me!");
             string userInput = Console.ReadLine();
-            
+            countTheVowels(userInput);
+            //reportTheVowels(userInput);
             /* 
              * 1) Split string into array by CHAR
              * 2) Loop through array and count IF char is a vowel
              * 3) Repeat 2 but store counts for each type of vowel for reporting
             */
+        }
+
+        private static void countTheVowels(string userInput)
+        {
+            int countOfVowels = 0;
+            int countofA = 0;
+            int countofE = 0;
+            int countofI = 0;
+            int countofO = 0;
+            int countofU = 0;
+
+            char[] splitInputTest = userInput.ToCharArray();
+            foreach (char c in splitInputTest)
+            {
+                char.ToLower(c);
+
+                switch (c)
+                {
+                    case 'a':
+                        countOfVowels += 1;
+                        countofA += 1;
+                        break;
+                    case 'e':
+                        countOfVowels += 1;
+                        countofE += 1;
+                        break;
+                    case 'i':
+                        countOfVowels += 1;
+                        countofI += 1;
+                        break;
+                    case 'o':
+                        countOfVowels += 1;
+                        countofO += 1;
+                        break;
+                    case 'u':
+                        countOfVowels += 1;
+                        countofU += 1;
+                        break;
+                }
+
+            }
         }
     }
 }
