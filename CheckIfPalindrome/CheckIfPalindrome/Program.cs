30d74c6e91bacdd1451d18bd4a83b226025bcbb9
OneDoesNotSimplyProgram <onedoesnotsimplyprogram@outlook.com>
2015-08-14 08:47:44 -0700

    Palindrome design notes
    Initial thoughts on how the design for this should work...

---
+++ CheckIfPalindrome/CheckIfPalindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIfPalindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, I'm a robot.  Bleep Bloop.  My job is to tell you if what you write is a Palindrome or not!");
+            Console.WriteLine("Please write something for my to analyze.");
+            string input = Console.ReadLine();
+            
+
+
+/*
+Palindromes are WORDS or PHRASES that read the same in both direction
+Word example:  Eye
+Phrase example:  Do geese see God?
+
+**Initial design**
+Get String (Do geese see God?)
+Strip special chars (Do geese see God)
+Strip spaces (DogeeseseeGod)
+ToLower (dogeeseseegod)
+Store ToLowererd as string1
+Reverse string1 and store as string2
+Compare string 1 to string 2 by length and value?
+*/
+
+        }
+    }
+}
